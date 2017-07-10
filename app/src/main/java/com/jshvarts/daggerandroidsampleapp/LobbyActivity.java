ec38a63ad8203b4ffde728ed741bd9fe0deed8c0
James Shvarts <jshvarts@gmail.com>
2017-07-10 15:26:22 -0400

    setup project

---
+++ app/src/main/java/com/jshvarts/daggerandroidsampleapp/LobbyActivity.java
@@ -0,0 +1,13 @@
+package com.jshvarts.daggerandroidsampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lobby_activity);
+    }
+}
