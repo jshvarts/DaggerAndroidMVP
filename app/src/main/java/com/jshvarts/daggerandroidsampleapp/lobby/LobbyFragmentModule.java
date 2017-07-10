05dfcefcb562f5c764843d116b47475602540a63
James Shvarts <jshvarts@gmail.com>
2017-07-10 17:46:15 -0400

    document gaining access to activity dependencies from fragment

--- app/src/main/java/com/jshvarts/daggerandroidsampleapp/lobby/LobbyFragmentModule.java
+++ app/src/main/java/com/jshvarts/daggerandroidsampleapp/lobby/LobbyFragmentModule.java
@@ -5,6 +5,9 @@ import dagger.Provides;
 
 /**
  * Define LobbyFragment-specific dependencies here.
+ *
+ * Note: If you need access to dependencies injected by the parent activity, extend {@link LobbyActivityModule}.
+ *
  */
 @Module
 public class LobbyFragmentModule {
